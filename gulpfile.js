c8ac5ccc5376b72dbae1d522db586e1026133a4d
Vitezslav Benes <vitezslav.benes@gmail.com>
2015-10-11 13:21:10 +0200

    Cleaned files and ported to gulp with browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+		plumber = require('gulp-plumber'),
+		sourcemaps = require('gulp-sourcemaps'),
+		sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+		browserSync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+  gulp.src('css/newtab.scss')
+		.pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(sourcemaps.write())
+    .pipe(gulp.dest('css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+			server: {
+					baseDir: "./",
+					index: "newtab.html"
+			}
+	});
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('watch', function(){
+	gulp.watch('css/newtab.scss*', ['sass']);
+	gulp.watch('js/core.js').on('change', browserSync.reload);
+	gulp.watch('newtab.html').on('change', browserSync.reload);
+});
--- js/core.js
+++ js/core.js
@@ -1,6 +1,12 @@
 var body = document.getElementById('body'),
     noteSpace = document.getElementById('noteSpace'),
     noteDelay = 800;
+var loadNotes = function() {
+  // when working on localhost chrome.storage is undefined
+  var text = chrome.storage.sync.get('text', function(obj){
+    noteSpace.innerHTML = obj.text;
+  });
+};
 document.onkeypress = function(e){
   if (e.which == 13) {
     chrome.storage.sync.set({'text': noteSpace.innerHTML}, function(){
@@ -12,31 +18,3 @@ window.onload = function(){
   // delay for fast loading
   setTimeout(loadNotes, noteDelay);
 };
-var loadNotes = function() {
-  var text = chrome.storage.sync.get('text', function(obj){
-    noteSpace.innerHTML = obj.text;
-  });
-};
-// not doing anything with bookmarks yet
-window.onbeforeunload = function(e) {
-  chrome.tabs.getAllInWindow(null, function(tabs){
-    for (var i = 0; i < tabs.length; i++) {
-      localStorage['tabs'+i] = tabs[i].url;
-    }
-  });
-};
-function processBookmarks(bookmarks) {
-    for (var i = 0; i < bookmarks.length; i++) {
-        var bookmark = bookmarks[i];
-        if (bookmark.url) console.log('bookmark: '+ bookmark.title + ' ~  ' + bookmark.url);
-        if (bookmark.children) process_bookmark(bookmark.children);
-    }
-}
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('listing bookmarks: ' );
-  try {
-    chrome.bookmarks.getTree(processBookmarks);
-  } catch (e) {
-    console.error('following error has occured: ' + e);
-  }
-});
